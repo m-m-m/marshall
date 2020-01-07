fb2a00344b8d2244ce102fb506f45760a078bb2d
Jörg Hohwiller <hohwille@users.sourceforge.net>
2020-01-07 22:28:17 +0100

    added package-infos

---
+++ impl/jsonp/src/main/java/io/github/mmm/marshall/jsonp/package-info.java
@@ -0,0 +1,6 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+/**
+ * Contains {@link io.github.mmm.marshall.jsonp.JsonpMarshalling} for JSON marshaling support based on JSON-P.
+ */
+package io.github.mmm.marshall.jsonp;
